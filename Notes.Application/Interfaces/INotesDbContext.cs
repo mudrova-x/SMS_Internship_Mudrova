9845949f007ecdd8648631d58d055b91dbd57230
mudrova-x <mudrova.x@gmail.com>
2022-04-07 22:06:32 +0400

    Entity Framework Core addition

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; } // коллекция всех сущностей в контексте (или запрашивается из бд заданного типа)
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken); // сохраняет изменения контекста в базу данных
+    }
+}
---
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
+
+
+namespace Notes.Persistence.EntityTypeConfigurations
+{
+    // извлечение всех конфигураций для типа сущности в класс NoteConfiguration
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        // реализуем конфигурацию для типа сущности с помощью EntityTypeBuilder
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+            // параметры конфигурации (+ ограничения полей)
+            // id - первичный ключ
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id).IsUnique();
+            builder.Property(note => note.Title).HasMaxLength(250);
+
+
+        }
+    }
+}
