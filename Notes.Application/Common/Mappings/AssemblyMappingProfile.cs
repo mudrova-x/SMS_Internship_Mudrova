2a9393bbb4d651e30d37903f7b6b527b08b6be3a
mudrova-x <ksenia.mudrova_external@wartsila.com>
2022-04-13 10:07:02 +0400

    automapper addition

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System.Reflection;
+using System.Linq;
+
+
+namespace Notes.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingFromAssebly(assembly);
+
+        // сканирует сборку и ищет любые типы, которые реализует IMapWith
+        private void ApplyMappingFromAssebly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            // вызывает метод mapping от наследуемого типа или интерфейса, если тип не реализует
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] {this});
+
+            }
+        }
+    }
+}
---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        // создает конфигурацию из исходного типа Т
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}
