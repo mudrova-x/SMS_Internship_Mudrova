617a93ea9852ea2e8d47ac438c0f23e604dc1a4c
mudrova-x <ksenia.mudrova_external@wartsila.com>
2022-04-19 21:58:30 +0400

    dependency injection

---
+++ Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistence
+{
+    // метод расширения для добавления контекста бд в приложение (добавляет и регистрирует)
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection 
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<NotesDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<INotesDbContext>(provider =>
+            provider.GetService<NotesDbContext>());
+            return services;
+        }
+    }
+}
