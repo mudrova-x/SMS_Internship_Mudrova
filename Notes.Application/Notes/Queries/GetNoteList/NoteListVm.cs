278282045bb257929f88292f49809ad98335780b
mudrova-x <ksenia.mudrova_external@wartsila.com>
2022-04-19 21:34:12 +0400

    queries added

---
+++ Notes.Application/Notes/Queries/GetNoteList/NoteListVm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class NoteListVm
+    {
+        public IList<NoteLookupDto> Notes { get; set; } 
+    }
+}
